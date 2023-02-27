0f983fc3485a5ff8a0e4ff0a022be0aba5b8d585
MinoriHaruta <hrtmnr.3072@gmail.com>
2023-02-27 15:05:12 +0900

    Done

--- app/src/main/java/com/haruta/harutyan/originalapp/MainActivity.kt
+++ app/src/main/java/com/haruta/harutyan/originalapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.haruta.harutyan.originalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.haruta.harutyan.originalapp.databinding.ActivityAddLocationBinding
+import com.haruta.harutyan.originalapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val addLocationIntent: Intent = Intent(this, AddLocationActivity::class.java)
+
+        binding.transitionFab.setOnClickListener {
+            startActivity(addLocationIntent)
+        }
+
     }
 }
