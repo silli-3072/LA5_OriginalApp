2a83a6374d0a6b470de8b151f2ee3dfe62cacc8a
MinoriHaruta <hrtmnr.3072@gmail.com>
2023-03-03 13:52:47 +0900

    位置情報の使用許可（実装途中 -with error②-）

--- app/src/main/java/com/haruta/harutyan/originalapp/AddLocationActivity.kt
+++ app/src/main/java/com/haruta/harutyan/originalapp/AddLocationActivity.kt
@@ -111,8 +111,6 @@ class AddLocationActivity : AppCompatActivity(),
     override fun onMyLocationButtonClick(): Boolean {
         Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
             .show()
-        // Return false so that we don't consume the event and the default behavior still occurs
-        // (the camera animates to the user's current position).
         return false
     }
 
