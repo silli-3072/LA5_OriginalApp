9521868133089c081401d3e15bb0c0e7c4438d37
MinoriHaruta <hrtmnr.3072@gmail.com>
2023-03-07 11:13:56 +0900

    レビューを受け修正

--- app/src/main/java/com/haruta/harutyan/originalapp/MainActivity.kt
+++ app/src/main/java/com/haruta/harutyan/originalapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.haruta.harutyan.originalapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.haruta.harutyan.originalapp.databinding.ActivityAddLocationBinding
 import com.haruta.harutyan.originalapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +13,6 @@ class MainActivity : AppCompatActivity() {
     var locationList: List<Location> = emptyList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        db = AppDatabase.getInstance(this.applicationContext)!!
-        locationList = db.locationDao().getAll()
-        Log.d("room",locationList.size.toString())
 
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
