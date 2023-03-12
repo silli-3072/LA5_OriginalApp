db15ee841e3f4b7f790fa23544a717d49599c495
MinoriHaruta <hrtmnr.3072@gmail.com>
2023-03-12 23:40:41 +0900

    エクスポートスキーマを有効にする

--- app/src/main/java/com/haruta/harutyan/originalapp/AppDatabase.kt
+++ app/src/main/java/com/haruta/harutyan/originalapp/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Location::class], version = 1, exportSchema = false)
+@Database(entities = [Location::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     // AppDatabaseにDaoを追加
     abstract fun locationDao(): LocationDao
